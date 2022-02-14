25638787dd839cdb905546efab765ae545068eaa
Abdulkader-Safi <safi.abdulkader@gmail.com>
2022-02-14 22:05:34 +0200

    update manage dashboard

--- app/Http/Controllers/Manage/ManageDashboardController.php
+++ app/Http/Controllers/Manage/ManageDashboardController.php
@@ -3,11 +3,33 @@
 namespace App\Http\Controllers\Manage;
 
 use App\Http\Controllers\Controller;
+use App\Models\Booking;
+use App\Models\Category;
+use App\Models\Order;
+use App\Models\Package;
+use App\Models\Product;
+use App\Models\User;
 use Inertia\Inertia;
 
 class ManageDashboardController extends Controller
 {
     public function index() {
-        return Inertia::render('Manage/Dashboard/Index');
+        $usersCount = User::count();
+        $ordersCount = Order::count();
+        $categoriesCount = Category::count();
+        $productsCount = Product::count();
+        $packagesCount = Package::count();
+        $bookingsCount = Booking::count();
+
+        $data = [
+            'users' => $usersCount,
+            'orders' => $ordersCount,
+            'categories' => $categoriesCount,
+            'products' => $productsCount,
+            'packages' => $packagesCount,
+            'bookings' => $bookingsCount
+        ];
+
+        return Inertia::render('Manage/Dashboard/Index', ['data' => $data]);
     }
 }
