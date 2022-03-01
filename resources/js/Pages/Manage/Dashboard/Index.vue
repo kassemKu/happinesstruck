a3695a237cf0518e3a6ac5a3c8cb5c87838d0422
kassemKu <arabisc95@gmail.com>
2022-03-01 10:58:42 +0200

    fix: merge confilect

--- resources/js/Pages/Manage/Dashboard/Index.vue
+++ resources/js/Pages/Manage/Dashboard/Index.vue
@@ -1,131 +1,143 @@
 <template>
   <ManageLayout>
     <template #breadcrumb>
-      <Breadcrumb 
+      <Breadcrumb
         :active-name="$t('dashboard')"
         active-icon="grid"
         :action-name="
           $t('action_model', { model: $t('dashboard'), action: $t('add') })
         "
-
       />
     </template>
 
-        <!-- :action-href="route('manage.dashboard.create')" -->
+    <!-- :action-href="route('manage.dashboard.create')" -->
 
     <div class="flex flex-col px-2 py-4 border-t-4 border-green-700 shadow-md">
-      <icon class="text-green-700 w-6 h-6 flex items-start justify-start " icon="bx:bxs-user"></icon>
+      <VueFeather
+        class="text-green-700 w-6 h-6 flex items-start justify-start"
+        type="user"
+      />
       <div class="flex flex-col py-2 items-center justify-center">
-        <p class="font-semibold"> {{data.users}} </p>
-        <p class="uppercase text-blue-400" >users</p> 
+        <p class="font-semibold">{{ data.users }}</p>
+        <p class="uppercase text-blue-400">users</p>
       </div>
       <div class="flex flex-col items-start justify-center">
-        <div class="flex items-center justify-start text-xs ">
-          <p class="text-gray-400"> last update at: </p>
-          <p class="font-semibold"> date </p>
+        <div class="flex items-center justify-start text-xs">
+          <p class="text-gray-400">last update at:</p>
+          <p class="font-semibold">date</p>
         </div>
-        <div class="flex items-center justify-start text-xs ">
-          <p class="text-gray-400"> the action: </p>
-          <p class="font-semibold"> the action </p>
+        <div class="flex items-center justify-start text-xs">
+          <p class="text-gray-400">the action:</p>
+          <p class="font-semibold">the action</p>
         </div>
       </div>
     </div>
 
     <div class="flex flex-col px-2 py-4 border-t-4 border-green-700 shadow-md">
-      <icon class="text-green-700 w-6 h-6 flex items-start justify-start " icon="bx:bxs-user"></icon>
+      <VueFeather
+        class="text-green-700 w-6 h-6 flex items-start justify-start"
+        type="user"
+      />
       <div class="flex flex-col py-2 items-center justify-center">
-        <p class="font-semibold"> {{data.orders}} </p>
-        <p class="uppercase text-blue-400" >orders</p> 
+        <p class="font-semibold">{{ data.orders }}</p>
+        <p class="uppercase text-blue-400">orders</p>
       </div>
       <div class="flex flex-col items-start justify-center">
-        <div class="flex items-center justify-start text-xs ">
-          <p class="text-gray-400"> last update at: </p>
-          <p class="font-semibold"> date </p>
+        <div class="flex items-center justify-start text-xs">
+          <p class="text-gray-400">last update at:</p>
+          <p class="font-semibold">date</p>
         </div>
-        <div class="flex items-center justify-start text-xs ">
-          <p class="text-gray-400"> the action: </p>
-          <p class="font-semibold"> the action </p>
+        <div class="flex items-center justify-start text-xs">
+          <p class="text-gray-400">the action:</p>
+          <p class="font-semibold">the action</p>
         </div>
       </div>
     </div>
 
     <div class="flex flex-col px-2 py-4 border-t-4 border-green-700 shadow-md">
-      <icon class="text-green-700 w-6 h-6 flex items-start justify-start " icon="bx:bxs-user"></icon>
+      <VueFeather
+        class="text-green-700 w-6 h-6 flex items-start justify-start"
+        type="user"
+      />
       <div class="flex flex-col py-2 items-center justify-center">
-        <p class="font-semibold"> {{data.categories}} </p>
-        <p class="uppercase text-blue-400" >categories</p> 
+        <p class="font-semibold">{{ data.categories }}</p>
+        <p class="uppercase text-blue-400">categories</p>
       </div>
       <div class="flex flex-col items-start justify-center">
-        <div class="flex items-center justify-start text-xs ">
-          <p class="text-gray-400"> last update at: </p>
-          <p class="font-semibold"> date </p>
+        <div class="flex items-center justify-start text-xs">
+          <p class="text-gray-400">last update at:</p>
+          <p class="font-semibold">date</p>
         </div>
-        <div class="flex items-center justify-start text-xs ">
-          <p class="text-gray-400"> the action: </p>
-          <p class="font-semibold"> the action </p>
+        <div class="flex items-center justify-start text-xs">
+          <p class="text-gray-400">the action:</p>
+          <p class="font-semibold">the action</p>
         </div>
       </div>
     </div>
 
     <div class="flex flex-col px-2 py-4 border-t-4 border-green-700 shadow-md">
-      <icon class="text-green-700 w-6 h-6 flex items-start justify-start " icon="bx:bxs-user"></icon>
+      <VueFeather
+        class="text-green-700 w-6 h-6 flex items-start justify-start"
+        type="user"
+      />
       <div class="flex flex-col py-2 items-center justify-center">
-        <p class="font-semibold"> {{data.products}} </p>
-        <p class="uppercase text-blue-400" >products</p> 
+        <p class="font-semibold">{{ data.products }}</p>
+        <p class="uppercase text-blue-400">products</p>
       </div>
       <div class="flex flex-col items-start justify-center">
-        <div class="flex items-center justify-start text-xs ">
-          <p class="text-gray-400"> last update at: </p>
-          <p class="font-semibold"> date </p>
+        <div class="flex items-center justify-start text-xs">
+          <p class="text-gray-400">last update at:</p>
+          <p class="font-semibold">date</p>
         </div>
-        <div class="flex items-center justify-start text-xs ">
-          <p class="text-gray-400"> the action: </p>
-          <p class="font-semibold"> the action </p>
+        <div class="flex items-center justify-start text-xs">
+          <p class="text-gray-400">the action:</p>
+          <p class="font-semibold">the action</p>
         </div>
       </div>
     </div>
 
     <div class="flex flex-col px-2 py-4 border-t-4 border-green-700 shadow-md">
-      <icon class="text-green-700 w-6 h-6 flex items-start justify-start " icon="bx:bxs-user"></icon>
+      <VueFeather
+        class="text-green-700 w-6 h-6 flex items-start justify-start"
+        type="user"
+      />
       <div class="flex flex-col py-2 items-center justify-center">
-        <p class="font-semibold"> {{data.packages}} </p>
-        <p class="uppercase text-blue-400" >packages</p> 
+        <p class="font-semibold">{{ data.packages }}</p>
+        <p class="uppercase text-blue-400">packages</p>
       </div>
       <div class="flex flex-col items-start justify-center">
-        <div class="flex items-center justify-start text-xs ">
-          <p class="text-gray-400"> last update at: </p>
-          <p class="font-semibold"> date </p>
+        <div class="flex items-center justify-start text-xs">
+          <p class="text-gray-400">last update at:</p>
+          <p class="font-semibold">date</p>
         </div>
-        <div class="flex items-center justify-start text-xs ">
-          <p class="text-gray-400"> the action: </p>
-          <p class="font-semibold"> the action </p>
+        <div class="flex items-center justify-start text-xs">
+          <p class="text-gray-400">the action:</p>
+          <p class="font-semibold">the action</p>
         </div>
       </div>
     </div>
 
     <div class="flex flex-col px-2 py-4 border-t-4 border-green-700 shadow-md">
-      <icon class="text-green-700 w-6 h-6 flex items-start justify-start " icon="bx:bxs-user"></icon>
+      <VueFeather
+        class="text-green-700 w-6 h-6 flex items-start justify-start"
+        type="user"
+      />
       <div class="flex flex-col py-2 items-center justify-center">
-        <p class="font-semibold"> {{data.bookings}} </p>
-        <p class="uppercase text-blue-400" >bookings</p> 
+        <p class="font-semibold">{{ data.bookings }}</p>
+        <p class="uppercase text-blue-400">bookings</p>
       </div>
       <div class="flex flex-col items-start justify-center">
-        <div class="flex items-center justify-start text-xs ">
-          <p class="text-gray-400"> last update at: </p>
-          <p class="font-semibold"> date </p>
+        <div class="flex items-center justify-start text-xs">
+          <p class="text-gray-400">last update at:</p>
+          <p class="font-semibold">date</p>
         </div>
-        <div class="flex items-center justify-start text-xs ">
-          <p class="text-gray-400"> the action: </p>
-          <p class="font-semibold"> the action </p>
+        <div class="flex items-center justify-start text-xs">
+          <p class="text-gray-400">the action:</p>
+          <p class="font-semibold">the action</p>
         </div>
       </div>
     </div>
-
-      
-  <!-- <pre> {{data.products}} </pre> -->
-=======
-    <div class="htm-dashboard-index grid grid-cols-4 gap-6"></div>
->>>>>>> e32635d22e9bc082febac31c1f97e24150bbf708
+    <!-- <pre> {{data.products}} </pre> -->
   </ManageLayout>
 </template>
 
@@ -133,9 +145,9 @@
 import ManageLayout from '@/Layouts/Manage/ManageLayout'
 import Breadcrumb from '@/Shared/Layouts/Breadcrumb'
 
-const components = { 
-  ManageLayout, 
-  Breadcrumb 
+const components = {
+  ManageLayout,
+  Breadcrumb,
 }
 
 export default {
@@ -146,13 +158,13 @@ export default {
     data: {
       type: Array,
       default: () => [],
-    }
+    },
   },
   setup(props) {
-      const dataDb = Object.entries(props.data)
-      return{
-          dataDb
-      }
-  }
+    const dataDb = Object.entries(props.data)
+    return {
+      dataDb,
+    }
+  },
 }
 </script>
